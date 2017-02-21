5ed2e845a6cc78de3af91047c4ca45964cbcd83b
ThangTruong <thangtm6@fsoft.com.vn>
2017-02-21 09:36:13 +0700

    Init

--- delegatePattern/ButtonViewController.swift
+++ delegatePattern/ButtonViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ViewControllerAfter.swift
 //  delegatePattern
 //
 //  Created by Trương Thắng on 2/21/17.
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ButtonViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func change(_ sender: Any) {
+        
+    }
+    
 }
+
+
+
 
---
+++ delegatePattern/ContainerVC.swift
@@ -0,0 +1,43 @@
+//
+//  ViewContainerController.swift
+//  delegatePattern
+//
+//  Created by Trương Thắng on 2/21/17.
+//  Copyright © 2017 Trương Thắng. All rights reserved.
+//
+
+import UIKit
+
+struct SegueIdentifier {
+    static let labelVC = "LabelViewController"
+    static let buttonVC = "ButtonViewController"
+}
+
+class ContainerVC: UIViewController {
+    var labelVC : LabelViewController?
+    var buttonVC : ButtonViewController?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier ?? "" {
+        case SegueIdentifier.labelVC:
+            labelVC = segue.destination as? LabelViewController
+            
+        case SegueIdentifier.buttonVC:
+            buttonVC = segue.destination as? ButtonViewController
+        default:
+            return
+        }
+    }
+    
+}
